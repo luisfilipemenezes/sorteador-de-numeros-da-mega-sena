80952447d9bb1de76b8a68295e63794508d0c1a6
luisfilipemenezes <86431670+luisfilipemenezes@users.noreply.github.com>
2023-01-24 22:02:04 -0300

    Create programa.js

---
+++ programa.js
@@ -0,0 +1,31 @@
+/*EM QUE TIPO DE JOGO VOCE VAI APOSTAR ?
+- MEGA SENA
+QUANTOS NUMEROS VOCE VAI APOSTAR ?
+-6
+*/
+
+function preenche (){
+    let valor = document.querySelector(".input")
+    valor.setAttribute("value" ,6)
+}
+
+function clicar (){
+    let volante  =[]
+    let cartao = document.querySelector("input")
+    let qtd =  cartao.value
+
+    for (let i = 0 ; i < qtd ; i++){
+        let numero = (parseInt(Math.random() * 20))
+        volante.push(numero)
+    }
+    console.log(volante)
+
+    let numeros =  document.querySelector("#result")
+    numeros.innerHTML = volante
+
+    
+
+}
+
+clicar()
+
